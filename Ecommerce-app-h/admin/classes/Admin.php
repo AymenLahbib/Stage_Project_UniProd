3d46172bda1e14c0f9c260d96905a7bb51c9cb1a
Aymen Lahbib <allahbib38@gmail.com>
2022-10-22 14:54:08 +0100

    stage

---
+++ Ecommerce-app-h/admin/classes/Admin.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+/**
+ * 
+ */
+class Admin
+{
+	
+	private $con;
+
+	function __construct()
+	{
+		include_once("Database.php");
+		$db = new Database();
+		$this->con = $db->connect();
+	}
+
+	public function getAdminList(){
+		$query = $this->con->query("SELECT id, name, email, is_active FROM `admin` WHERE 1");
+		$ar = [];
+		if ($query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				$ar[] = $row;
+			}
+			return ['status'=> 202, 'message'=> $ar];
+		}
+		return ['status'=> 303, 'message'=> 'No Admin'];
+	}
+
+	public function deleteAdmin($aid = null){
+		if ($aid != null) {
+			$q = $this->con->query("DELETE FROM admin WHERE admin_id = '$aid'");
+			if ($q) {
+				return ['status'=> 202, 'message'=> 'Product removed from stocks'];
+			}else{
+				return ['status'=> 202, 'message'=> 'Failed to run query'];
+			}
+			
+		}else{
+			return ['status'=> 303, 'message'=>'Invalid product id'];
+		}
+
+	}
+
+}
+
+
+if (isset($_POST['GET_ADMIN'])) {
+	$a = new Admin();
+	echo json_encode($a->getAdminList());
+	exit();
+	
+}
+
+if (isset($_POST['DELETE_ADMIN'])) {
+	$p = new Admin();
+	if (isset($_SESSION['admin_id'])) {
+		if(!empty($_POST['aid'])){
+			$aid = $_POST['aid'];
+			echo json_encode($p->deleteAdmin($aid));
+			exit();
+		}else{
+			echo json_encode(['status'=> 303, 'message'=> 'Invalid product id']);
+			exit();
+		}
+	}else{
+		echo json_encode(['status'=> 303, 'message'=> 'Invalid Session']);
+	}
+
+
+}
+
+?>
